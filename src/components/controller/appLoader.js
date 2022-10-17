d888c019321ba0254ac056cf45c40bc7098aef69
JiriSimonov <jirisimonov@gmail.com>
2022-10-17 14:16:19 +0300

    feat: add api key

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '8cc56851184349b7b541da66be86470d',
         });
     }
 }
